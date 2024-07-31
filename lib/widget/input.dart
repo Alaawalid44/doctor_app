29535ec2384edb91f7e97ec02cc8727917c41f28
Alaawalid44 <alaawalid442003@gmail.com>
2024-08-01 01:16:37 +0300

    Update input.dart

--- lib/widget/input.dart
+++ lib/widget/input.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class InputWidget extends StatelessWidget {
-  const InputWidget({
-    super.key,
-  });
+  const InputWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.grey.shade100, borderRadius: BorderRadius.circular(12)),
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.only(top: 10),
-      child: TextFormField(
-        decoration: InputDecoration(
-            hintText: "Search doctor or health issue",
-            prefixIcon: Container(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: SvgPicture.asset("assets/svg/search-normal.svg"),
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            hintText: 'Search doctor or health issue',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
             ),
-            border: InputBorder.none,
-            hintStyle: const TextStyle(
-              fontSize: 15,
-              color: Color(0xFF8696BB),
-            )),
-      ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.coronavirus, size: 40, color: Colors.blue),
+                const SizedBox(height: 5),
+                Text('Covid 19'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.person, size: 40, color: Colors.blue),
+                const SizedBox(height: 5),
+                Text('Doctor'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.local_pharmacy, size: 40, color: Colors.blue),
+                const SizedBox(height: 5),
+                Text('Medicine'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.local_hospital, size: 40, color: Colors.blue),
+                const SizedBox(height: 5),
+                Text('Hospital'),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
+
