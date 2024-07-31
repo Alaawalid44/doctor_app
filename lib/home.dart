9f3b3240b96c90dab66188b7055f6a7a07d8898e
Alaawalid44 <alaawalid442003@gmail.com>
2024-08-01 01:01:06 +0300

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,24 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:doctor_app/widget/doctor.dart';
 import 'package:doctor_app/widget/header.dart';
 import 'package:doctor_app/widget/input.dart';
-import 'package:flutter/material.dart';
+import 'package:doctor_app/widget/nearby_doctor.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({
-    super.key,
-  });
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const HeaderWidget(),
+              const SizedBox(height: 20),
               const DoctorWidget(),
+              const SizedBox(height: 20),
               const InputWidget(),
+              const SizedBox(height: 20),
+              const Text(
+                'Near Doctor',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Expanded(child: NearbyDoctorWidget()),
             ],
           ),
         ),
